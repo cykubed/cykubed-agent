61e10fcd44663d8506ad697dfa5d8b3be18a9ca4
nick <nick@kisanhub.com>
2021-08-21 16:44:29 +0100

    good failure test case

--- scripts/testrun.sh
+++ scripts/testrun.sh
@@ -1,4 +1,4 @@
-http post http://localhost:5000/api/start branch=force-fail sha=f52d0accf0f73b48d9d30ca9f9f101c78c7b68b9 repos=kisanhubcore/kisanhub-webapp
+http post http://localhost:5000/api/start branch=force-fail sha=dc60660ce0429832e75ed29f6edf4b0520b5f013 repos=kisanhubcore/kisanhub-webapp
 
  #docker run -v /projects/secrets/kisanhub-uat-8e82a4304002.json:/cred.json -e GOOGLE_APPLICATION_CREDENTIALS=/cred.json -e COMMIT_SHA=$COMMIT_SHA -e CYPRESS_HUB=192.168.1.126:5000  gcr.io/kisanhub-uat/cypress-runner:latest
 
