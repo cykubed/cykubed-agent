33874dbe303afd452e01ac091fefa9f64133ec31
nick <nickbrook72@gmail.com>
2023-01-03 18:50:05 +0000

    latest token

--- scripts/install-helm.sh
+++ scripts/install-helm.sh
@@ -1,7 +1,7 @@
 poetry export -o requirements.txt
 eval "$(minikube docker-env)"
 docker build . -t cykube/agent:$1
-helm upgrade --install cykube -n cykube --create-namespace --set-string token=19bb4c8a-40a0-4ceb-8a73-ab9f712a6598 --set-string agentVersion="$1" ./chart
+helm upgrade --install cykube -n cykube --create-namespace --set-string token=1da3b1a6-1027-4144-afdd-28849e5b38ce --set-string agentVersion="$1" ./chart
 
 
 
