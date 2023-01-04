ec2a1c0f22596edbba00a4b2ef0f4a8fb97bb6fe
nick <nick@kisanhub.com>
2023-01-04 19:06:16 +0000

    Added support for pagekite Pass in URL as well

---
+++ scripts/install-helm-pagekite.sh
@@ -0,0 +1,7 @@
+poetry export -o requirements.txt
+eval "$(minikube docker-env)"
+docker build . -t cykube/agent:$1
+helm upgrade --install cykube -n cykube --create-namespace --set-string token=d5be81e0-da0c-4172-97dc-5d473d75b2ff --set-string agentVersion="$1" --set-string apiUrl=https://cykube.pagekite.me/api ./chart
+
+
+
