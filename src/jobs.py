2328bb88f0d83e06b0f8d482764ca7506fa9f734
nick <nick@kisanhub.com>
2023-01-06 14:47:16 +0000

    move template creation code to common default to k8

--- src/jobs.py
+++ src/jobs.py
@@ -1,9 +1,10 @@
-from kubernetes import client
-from loguru import logger
+import os.path
 
 from common import schemas
-from common.k8common import NAMESPACE, get_batch_api, get_job_env
+from common.k8common import NAMESPACE, get_batch_api, create_jobs
 from common.logupload import upload_log_line
+
+TEMPLATE_FILES = open(os.path.join(os.path.dirname(__file__), 'k8config/build-job.yaml')).read().split('---')
 
 
 def delete_jobs_for_branch(trid: int, branch: str):
@@ -20,42 +21,6 @@ def delete_jobs_for_branch(trid: int, branch: str):
 
 
 def create_build_job(testrun: schemas.NewTestRun):
-    """
-    Create a Job to clone and build the app
-    :param testrun:
-    :return:
-    """
-    job_name = f'cykube-build-{testrun.id}'
-    container = client.V1Container(
-        image=testrun.project.runner_image,
-        name='cykube-builder',
-        image_pull_policy='IfNotPresent',
-        env=get_job_env(),
-        resources=client.V1ResourceRequirements(
-            requests={"cpu": testrun.project.build_cpu,
-                      "memory": testrun.project.build_memory,
-                      "ephemeral-storage": "2Gi"},
-            limits={"cpu": testrun.project.build_cpu,
-                    "memory": testrun.project.build_memory,
-                    "ephemeral-storage": "4Gi"}
-        ),
-        args=["build", str(testrun.id)],
-    )
-    pod_template = client.V1PodTemplateSpec(
-        spec=client.V1PodSpec(restart_policy="Never",
-                              containers=[container]),
-        metadata=client.V1ObjectMeta(name='cykube-builder')
-    )
-    metadata = client.V1ObjectMeta(name=job_name,
-                                   labels={"cykube-job": "builder",
-                                           "branch": testrun.branch})
-
-    job = client.V1Job(
-        api_version="batch/v1",
-        kind="Job",
-        metadata=metadata,
-        spec=client.V1JobSpec(backoff_limit=0, template=pod_template,
-                              ttl_seconds_after_finished=3600),
-    )
     upload_log_line(testrun.id, "Creating build job")
-    get_batch_api().create_namespaced_job(NAMESPACE, job)
+    create_jobs(TEMPLATE_FILES, testrun)
+    # TODO (maybe) track job progress?
--- src/settings.py
+++ src/settings.py
@@ -12,7 +12,9 @@ class JobMode(str, enum.Enum):
 class AppSettings(BaseSettings):
     API_TOKEN: str = 'cykubeauth'
 
-    JOB_MODE: JobMode = 'inline'
+    JOB_MODE: JobMode = 'k8'
+
+    NAMESPACE = 'cykube'
 
     TEST_RUN_TIMEOUT: int = 30 * 60
     SPEC_FILE_TIMEOUT: int = 5 * 60
@@ -23,7 +25,7 @@ class AppSettings(BaseSettings):
     TEST_MODE: bool = True
     PARALLELISM: int = 1
 
-    HUB_URL: str = 'http://127.0.0.1:5000'
+    AGENT_URL: str = 'http://127.0.0.1:5000'
     CACHE_URL: str = 'http://127.0.0.1:5001'
 
     MAIN_API_URL: str = 'https://app.cykube.net/api'
