7e181709ef20b39c8b5982067c7dce43aa514b86
nick <nickbrook72@gmail.com>
2023-03-05 10:52:40 +0000

    Remove stackdriver integration: it's not worth the hassle

--- src/logs.py
+++ src/logs.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import loguru
 from loguru import logger
 
-from common.cloudlogging import configure_stackdriver_logging
 from messages import queue
 
 
@@ -25,4 +24,3 @@ def rest_logsink(msg: loguru.Message):
 def configure_logging():
     logger.add(rest_logsink,
                format="{message}", level="INFO")
-    configure_stackdriver_logging('cykube-agent')
