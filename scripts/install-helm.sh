688a0b4ccc6e6c68eb3591bc1b57a30e339fb26d
nick <nickbrook72@gmail.com>
2023-03-06 12:07:53 +0000

    Make the names more descriptive

--- scripts/install-helm.sh
+++ scripts/install-helm.sh
@@ -1,6 +1,6 @@
 eval "$(minikube docker-env)"
 docker build . -t nickbrookck/cykube-agent:$1
-helm upgrade --install cykube -n cykube --create-namespace --set token=$2 --set agentName=Minikube --set agentImage="nickbrookck/cykube-agent:$1" ./chart
+helm upgrade --install cykube -n cykube --create-namespace --set token=$2 --set agentName=Minikube --set agentImage="nickbrookck/cykube-agent:$1" --set apiUrl="https://dev.cykube.net/api" ./chart
 
 
 
