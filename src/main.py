f94cfbc85243ca8eda9f1c1ebde494f707ce8b59
nick <nickbrook72@gmail.com>
2023-03-15 12:44:01 +0000

    shunt stuff around and add Redis integration to Sentry

--- src/main.py
+++ src/main.py
@@ -4,21 +4,17 @@ import os
 import sentry_sdk
 from loguru import logger
 from sentry_sdk.integrations.asyncio import AsyncioIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
 
 import jobs
 import messages
 import ws
 from common import k8common, db
-from common.db import async_redis, sync_redis
+from common.db import redis
 from common.enums import AgentEventType
 from common.schemas import AgentEvent, AgentCompletedBuildMessage
 from common.settings import settings
 from logs import configure_logging
-
-if os.environ.get('SENTRY_DSN'):
-    sentry_sdk.init(integrations=[
-        AsyncioIntegration(),
-    ],)
 
 
 async def poll_messages(max_messages=None):
@@ -27,7 +23,7 @@ async def poll_messages(max_messages=None):
     :param max_messages: limit the number of messages sent (for unit testing)
     """
     sent = 0
-    async with async_redis().pubsub() as pubsub:
+    async with redis().pubsub() as pubsub:
         await pubsub.psubscribe('messages')
         while True:
             message = await pubsub.get_message(ignore_subscribe_messages=True, timeout=1.0)
@@ -37,30 +33,32 @@ async def poll_messages(max_messages=None):
                 messages.queue.add(msg)
                 if event.type == AgentEventType.build_completed:
                     buildmsg: AgentCompletedBuildMessage = AgentCompletedBuildMessage.parse_raw(msg)
-                    tr = await db.build_completed(buildmsg)
+                    tr = await db.get_testrun(buildmsg.testrun_id)
                     jobs.create_runner_jobs(tr, buildmsg)
                 sent += 1
                 if max_messages and sent == max_messages:
+                    # for easier testing
                     return
-
-
-# async def cleanup_cache_poll():
-#     while True:
-#         await asyncio.sleep(300)
-#         await cleanup_cache()
 
 
 async def init():
     """
     Run the websocket and server concurrently
     """
-    # block until we can access Redis
-    sync_redis()
-
-    await asyncio.gather(ws.connect(), poll_messages()) #, cleanup_cache_poll())
+    await asyncio.gather(ws.connect(), poll_messages())
 
 
 if __name__ == "__main__":
+
+    if os.environ.get('SENTRY_DSN'):
+        sentry_sdk.init(integrations=[
+            RedisIntegration(),
+            AsyncioIntegration(),
+        ], )
+
+    # block until we can access Redis
+    redis()
+
     try:
         if settings.K8 and not settings.TEST:
             k8common.init()
