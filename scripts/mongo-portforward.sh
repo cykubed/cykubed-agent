bd132925778bab37e7bd661bece82a2187365a34
nick <nickbrook72@gmail.com>
2023-03-17 16:01:47 +0000

    First cut at chart

--- scripts/mongo-portforward.sh
+++
@@ -1,1 +0,0 @@
-kubectl port-forward -n cykube cykube-mongodb-0 27018:27017
--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
+import argparse
 import asyncio
 import os
+import sys
 
 import sentry_sdk
 from loguru import logger
 from sentry_sdk.integrations.asyncio import AsyncioIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
 
+import fsserver
 from common import k8common
 from common.db import sync_redis
 from common.settings import settings
@@ -20,16 +23,28 @@ if __name__ == "__main__":
             AsyncioIntegration(),
         ], )
 
-    # block until we can access Redis
-    sync_redis()
+    parser = argparse.ArgumentParser('CykubeAgent')
+    parser.add_argument('--port', type=int, default=8100, help='Port')
+    parser.add_argument('command', choices=['agent', 'fs'], help='Command')
+    args = parser.parse_args()
 
-    try:
-        if settings.K8 and not settings.TEST:
-            k8common.init()
-        configure_logging()
-        asyncio.run(init())
-    except KeyboardInterrupt:
-        shutdown()
-    except Exception as ex:
-        logger.exception("Agent quit expectedly")
+    if args.command == 'agent':
+        # block until we can access Redis
+        sync_redis()
 
+        try:
+            if settings.K8 and not settings.TEST:
+                k8common.init()
+            configure_logging()
+            asyncio.run(init())
+        except KeyboardInterrupt:
+            shutdown()
+        except Exception as ex:
+            logger.exception("Agent quit expectedly")
+            sys.exit(1)
+    else:
+        try:
+            fsserver.start(args.port)
+        except Exception as ex:
+            logger.exception("Filestore quit expectedly")
+            sys.exit(1)
