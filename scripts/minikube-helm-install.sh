d493ab5ee71883a8d1c3a38189a60da9f0088231
nick <nickbrook72@gmail.com>
2023-03-18 17:57:51 +0000

    FS now exports port 8000

--- scripts/minikube-helm-install.sh
+++ scripts/minikube-helm-install.sh
@@ -1,7 +1,7 @@
 eval "$(minikube docker-env)"
-docker build -f dockerfiles/agent/Dockerfile . -t nickbrookck/cykube-agent:$2
+docker build  . -t nickbrookck/cykube-agent:$2
 helm upgrade --install cykube -n cykube --create-namespace --set token=$1 --set agentName=Minikube --set tag="$2" --set apiUrl="https://dev.cykube.net/api" ./chart
-#kubectl rollout status deployment cykube-agent
+kubectl rollout status deployment cykube-agent
 kubectl rollout status statefulsets fs
 
 
