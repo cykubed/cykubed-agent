9415e172ec36e119887cf9707b067b119e863148
nick <nickbrook72@gmail.com>
2023-03-19 17:11:48 +0000

    explicit types increase timeouts

--- scripts/gkehelm.sh
+++ scripts/gkehelm.sh
@@ -1,2 +1,4 @@
 helm upgrade --install cykube -n cykube --create-namespace --set token=$1 --set agentName=GKE --set tag="$2" --set apiUrl="https://dev.cykube.net/api" ./chart
-kubectl rollout status deployment
+kubectl config set-context --current --namespace=cykube
+kubectl rollout status deployment cykube-agent
+kubectl rollout status statefulsets fs
