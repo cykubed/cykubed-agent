83a6220370f1590d602b7eacfd94fc725a1df2a2
nick <nickbrook72@gmail.com>
2023-03-27 11:10:30 +0100

    New cluster

--- scripts/gkehelm.sh
+++ scripts/gkehelm.sh
@@ -1,3 +1,3 @@
-helm upgrade --install cykube -n cykube --create-namespace --set token=$1 --set agentName=GKE --set tag="$2" --set apiUrl="https://dev.cykube.net/api" ./chart
+helm upgrade --install cykube -n cykube --create-namespace --set token=$1 --set agentName=GKE-S --set tag="$2" --set apiUrl="https://api.cykube.net" ./chart
 kubectl config set-context --current --namespace=cykube
 kubectl rollout status statefulsets fs
