b073d03807515834499130a16792b36f5c24eabc
nick <nickbrook72@gmail.com>
2023-03-30 17:25:52 +0100

    sigh - submodule again

--- scripts/kh-helm.sh
+++ scripts/kh-helm.sh
@@ -1,3 +1,5 @@
+gcloud container clusters get-credentials europe --region europe-west2 --project kisanhub-uat
+gcloud config set project kisanhub-uat
 helm upgrade --install cykube -n cykube --create-namespace --set token=e7714eed-ae86-4324-9fae-ad6faf76cfe6 --set agentName=GKE-KH --set tag="$1" --set apiUrl="https://api.cykube.net" ./chart
 kubectl config set-context --current --namespace=cykube
 kubectl rollout status statefulsets fs
