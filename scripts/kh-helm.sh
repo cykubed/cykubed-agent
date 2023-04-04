4025ed89cd89c5bb0f72b5220a628055ee847002
nick <nickbrook72@gmail.com>
2023-04-04 10:30:51 +0100

    moved the exception here

--- scripts/kh-helm.sh
+++ scripts/kh-helm.sh
@@ -1,5 +1,5 @@
 #gcloud container clusters get-credentials europe --region europe-west2 --project kisanhub-uat
 #gcloud config set project kisanhub-uat
-helm upgrade --install cykube -n cykube --create-namespace --set token=15ad7b82-ecd5-476b-ad9d-1f6c6d38ed1d --set agentName=GKE-KH --set tag="$1" --set apiUrl="https://api.cykube.net" ./chart
+helm upgrade --install cykube -n cykube --create-namespace --set token=$1 --set agentName=GKE-KH --set tag="$2" --set apiUrl="https://api.cykube.net" ./chart
 kubectl config set-context --current --namespace=cykube
 kubectl rollout status statefulsets fs
--- scripts/kisanhub-install.sh
+++
@@ -1,3 +0,0 @@
-helm upgrade --install cykube -n cykube --create-namespace --set token=e7714eed-ae86-4324-9fae-ad6faf76cfe6 --set agentName=GKE-S --set tag="$1" --set apiUrl="https://api.cykube.net" ./chart
-kubectl config set-context --current --namespace=cykube
-kubectl rollout status statefulsets fs
