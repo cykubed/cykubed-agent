3f7b15c74362bf1b3a2dcaf999efb89c3d963525
nick <nickbrook72@gmail.com>
2023-04-05 13:53:45 +0100

    sigh

--- scripts/cloudbuild/build-chart.sh
+++ scripts/cloudbuild/build-chart.sh
@@ -3,4 +3,4 @@ mkdir -p /workspace/build/chart
 mkdir -p /workspace/build/dist
 cp -fr /workspace/chart/* /workspace/build/chart
 sed -i "s/IMAGE_TAG/$VERSION/g" /workspace/build/chart/values.yaml
-helm package /workspace/build/chart -d build/dist --app-version "$VERSION" --version "$VERSION"
+helm package /workspace/build/chart -d /workspace/build/dist --app-version "$VERSION" --version "$VERSION"
