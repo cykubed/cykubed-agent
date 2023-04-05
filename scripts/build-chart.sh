c678ed46bf6557c55e80853e16e77a30e378d0fc
nick <nickbrook72@gmail.com>
2023-04-05 11:49:02 +0100

    Now uses GCR

---
+++ scripts/build-chart.sh
@@ -0,0 +1,6 @@
+rm -fr build/*
+mkdir -p build/chart
+mkdir -p build/dist
+cp -fr chart/* build/chart
+sed -i "s/IMAGE_TAG/$VERSION/g" build/chart/values.yaml
+helm package build/chart -d build/dist --app-version "$VERSION" --version "$VERSION"
