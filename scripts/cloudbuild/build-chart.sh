f386bf6350d2cdd218d703a0af617f09334dda59
nick <nickbrook72@gmail.com>
2023-04-05 14:20:39 +0100

    Fixed Slack notification

--- scripts/cloudbuild/build-chart.sh
+++ scripts/cloudbuild/build-chart.sh
@@ -5,3 +5,5 @@ cp -fr /workspace/chart/* /workspace/build/chart
 sed -i "s/IMAGE_TAG/$VERSION/g" /workspace/build/chart/values.yaml
 helm package /workspace/build/chart -d /workspace/build/dist --app-version "$VERSION" --version "$VERSION"
 helm repo index /workspace/build/dist --url https://charts.cykube.net
+echo "{\"text\":\"Cykube agent $VERSION published\"}" > /workspace/notify.json
+
