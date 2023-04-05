0de8f53b04a88e9049a8c41ee8f6217257fafded
nick <nickbrook72@gmail.com>
2023-04-05 14:00:19 +0100

    missed this

--- scripts/cloudbuild/build-chart.sh
+++ scripts/cloudbuild/build-chart.sh
@@ -4,3 +4,4 @@ mkdir -p /workspace/build/dist
 cp -fr /workspace/chart/* /workspace/build/chart
 sed -i "s/IMAGE_TAG/$VERSION/g" /workspace/build/chart/values.yaml
 helm package /workspace/build/chart -d /workspace/build/dist --app-version "$VERSION" --version "$VERSION"
+helm repo index /workspace/build/dist --url https://charts.cykube.net
