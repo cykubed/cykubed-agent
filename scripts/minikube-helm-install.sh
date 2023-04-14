94ab75d69985e0f8924e09109c82dedd7fb7f19f
nick <nickbrook72@gmail.com>
2023-04-14 13:43:12 +0100

    Added test automation

--- scripts/minikube-helm-install.sh
+++ scripts/minikube-helm-install.sh
@@ -1,6 +1,6 @@
-eval "$(minikube docker-env)"
 docker build  . -t europe-west2-docker.pkg.dev/cykubeapp/cykube/agent:$2
-helm upgrade --install cykube -n cykube --create-namespace --set token=$1 --set tag="$2" --set apiUrl="https://dev.cykubed.com/api"  --set cache.storageClass=standard ./chart
+minikube image load europe-west2-docker.pkg.dev/cykubeapp/cykube/agent:$2
+helm upgrade --install cykube -n cykube --create-namespace --set token=$1 --set tag="$2" --set apiUrl="https://dev.cykubed.com/api"  --set cache.storageClass=standard --set imagePullPolicy=IfNotPresent ./chart
 kubectl config set-context --current --namespace=cykube
 kubectl rollout status statefulsets agent
 
