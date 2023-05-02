9a554724f6de7e2dde165a18bb76d645dfed5f19
nick <nickbrook72@gmail.com>
2023-05-02 14:32:21 +0100

    Fixed singleton redis stuff

--- scripts/minikube-helm-install-singleton.sh
+++ scripts/minikube-helm-install-singleton.sh
@@ -3,8 +3,8 @@ TOKEN=$(mysql cykubemain -e "select token from agent where name='minikube'" --sk
 docker build  . -t europe-west2-docker.pkg.dev/cykubeapp/cykube/agent:"$TAG"
 minikube image load europe-west2-docker.pkg.dev/cykubeapp/cykube/agent:"$TAG"
 
-helm upgrade --install cykube -n cykube --create-namespace --set token="$TOKEN" --set tag="$TAG" --set apiUrl="https://dev.cykubed.com/api"  --set cache.storageClass=standard --set redis.sentinel.enabled=false --set redis.architecture=standalone --set redis.replica.replicaCount=1  ./chart
-kubectl config set-context --current --namespace=cykube
+helm upgrade --install cykubed -n cykubed --create-namespace --set token="$TOKEN" --set tag="$TAG" --set apiUrl="https://dev.cykubed.com/api"  --set platform=minikube --set imagePullPolicy=IfNotPresent --set cache.storageClass=standard --set redis.sentinel.enabled=false --set redis.architecture=standalone  ./chart
+kubectl config set-context --current --namespace=cykubed
 kubectl rollout status statefulsets agent
 
 
--- scripts/singleton-redis-portforward.sh
+++ scripts/singleton-redis-portforward.sh
@@ -1,1 +1,4 @@
-kubectl port-forward --namespace cykubed svc/redis-master 6380:6379
+export REDIS_PASSWORD=$(kubectl get secret cykubed-redis --namespace cykubed -o jsonpath="{.data.redis-password}" | base64 -d)
+echo "Redis password=$REDIS_PASSWORD"
+kubectl port-forward --namespace cykubed svc/cykubed-redis-master 6380:6379
+
