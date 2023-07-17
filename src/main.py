a1ab530ad9c42f110adcb1ba13b37be8887643cd
nick <nickbrook72@gmail.com>
2023-07-17 22:16:18 +0100

    job tracker is borked - remove for now

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from app import app
 from common import k8common
 from common.k8common import close
 from common.redisutils import sync_redis, ping_redis, async_redis
-from jobs import prune_cache_loop, watch_pod_events, watch_job_events
+from jobs import prune_cache_loop, watch_pod_events
 from logs import configure_logging
 from settings import settings
 
@@ -40,9 +40,8 @@ async def run():
              asyncio.create_task(ws.connect())]
     if app.hostname == 'agent-0':
         tasks += [asyncio.create_task(prune_cache_loop()),
-                  # asyncio.create_task(run_job_tracker()),
                   asyncio.create_task(watch_pod_events()),
-                  asyncio.create_task(watch_job_events()),
+                  # asyncio.create_task(watch_job_events()), # This is broken!
                   ]
     done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
     # cancel the others
