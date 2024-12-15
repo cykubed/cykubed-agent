e923ca5bac807de219c8b2c8040bb9157023970b
nick <nick@myrecovery.ai>
2024-12-15 11:03:16 +0000

    remove master check for now

--- release.py
+++ release.py
@@ -19,11 +19,11 @@ def cmd(args: str, silent=False) -> str:
 
 @click.command(help='Generate a new release of the runner')
 @click.option('-b', '--bump', type=click.Choice(['major', 'minor', 'patch']),
-              default='minor', help='Type of version bump')
+              default='patch', help='Type of version bump')
 def generate(bump: str):
 
-    if cmd('git branch --show-current') != MAIN_BRANCH:
-        raise click.BadParameter('Not on master branch')
+    # if cmd('git branch --show-current') != MAIN_BRANCH:
+    #     raise click.BadParameter('Not on master branch')
 
     # bump and get the tag
     tag = cmd(f"poetry version {bump} -s")
@@ -32,7 +32,7 @@ def generate(bump: str):
     cmd(f'git add pyproject.toml')
     cmd(f'git commit -m "New release {tag}"')
     cmd(f'git tag -a {tag} -m "New release:\n{tag}"')
-    cmd(f'git push origin {MAIN_BRANCH} --tags')
+    cmd(f'git push origin tag {tag}')
 
 
 if __name__ == '__main__':
